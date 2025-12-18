3a525d69d22ee5b1cb7525988baf66b608efe639
sandy <sandydwi75@gmail.com>
2025-12-18 14:13:17 +0700

    feat: find second largest

--- src/main/java/belajar/java/sorting/dan/searching/FindSecondLargestApp.java
+++ src/main/java/belajar/java/sorting/dan/searching/FindSecondLargestApp.java
@@ -2,10 +2,33 @@ package belajar.java.sorting.dan.searching;
 
 public class FindSecondLargestApp {
     public static void main(String[] args) {
+        int[] numbers1 = new int[]{ 2, 3, 1, 5, 3, 7, 8, 10 };
+        int[] numbers2 = new int[]{ 4, 3, 1, 2, 6, 7, 4, 2, 12 };
+        int[] numbers3 = new int[]{ 10, 11, -2, -1, -21, 0 };
 
+        System.out.println("Numbers 1");
+        System.out.println(findSecondLargest(numbers1));
+
+        System.out.println("Numbers 2");
+        System.out.println(findSecondLargest(numbers2));
+
+        System.out.println("Numbers 3");
+        System.out.println(findSecondLargest(numbers3));
     }
 
     public static int findSecondLargest(int[] arr) {
-        return -1;
+        int max = 0;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int number : arr) {
+            if (number > max) {
+                secondMax = max;
+                max = number;
+            } else if (number < max && number > secondMax) {
+                secondMax = number;
+            }
+        }
+
+        return secondMax;
     }
 }
